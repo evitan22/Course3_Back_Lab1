a0cc59930d76ce2abfac42728b82df571cc6d3df
evitan22 <kokravets@gmail.com>
2026-03-22 23:40:24 +0200

    changed users to students

--- server.js
+++ server.js
@@ -5,30 +5,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let users = [];
+let students = [];
 
-app.get('/users', (req, res) => { 
-    res.json(users);
+app.get('/students', (req, res) => { 
+    res.json(students);
 });
 
-app.post('/users', (req, res) => {
-    const user = req.body;
-    users.push(user);
-    res.json(user);
+app.post('/students', (req, res) => {
+    const student = req.body;
+    students.push(student);
+    res.json(student);
 });
 
-app.delete('/users/:id', (req, res) => {
+app.delete('/students/:id', (req, res) => {
     const id = req.params.id; 
-    users = users.filter(u => u.id != id); 
-    res.send(`User ${id} was deleted`);
+    students = students.filter(u => u.id != id); 
+    res.send(JSON.stringify(`Student ${id} was deleted`));
 });
 
-app.put('/users/:id', (req, res) => {
-    const user = req.body;
+app.put('/students/:id', (req, res) => {
+    const student = req.body;
     const id = req.params.id; 
-    const userIndex = users.findIndex(u => u.id == id);
-    users.splice(userIndex, 1, user);
-    res.send(`Data for user ${id} was changed`);
+    const studentIndex = students.findIndex(u => u.id == id);
+    students.splice(studentIndex, 1, student);
+    res.send(JSON.stringify(`Data for Student ${id} was changed`));
 });
 
 app.listen(3000, () => {
